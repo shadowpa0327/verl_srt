fc2c9f71314116497f6a7376b58b9467185af842
chichih.chang <chichih.chang@bytedance.com>
2025-11-06 13:02:17 +0800

    fix oom

--- recipe/srt/qwen_2_7b_vllm_MATH_no_spec_o4k.sh
+++ recipe/srt/qwen_2_7b_vllm_MATH_no_spec_o4k.sh
@@ -9,6 +9,8 @@ math_test_path=/mnt/hdfs/ccchang_hldy/data/math/test.parquet
 train_files="['$math_train_path']"
 test_files="['$math_test_path']"
 
+use_dynamic_bsz=True
+
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files="$train_files" \
@@ -21,27 +23,29 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path="/mnt/hdfs/ccchang_hldy/Qwen2-7B-Instruct" \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=40 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.param_offload=False \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=40 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.disable_log_stats=False \
     actor_rollout_ref.rollout.n=5 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=40 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
+    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
+    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
-    trainer.project_name='verl_grpo_example_gsm8k_math_cc' \
+    trainer.project_name='verl_grpo_example_gsm8k_math_cc' \actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
+    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
+    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
     trainer.experiment_name='qwen2_7b_1104_no_spec_bs1024_o4k' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
--- recipe/srt/qwen_2_7b_vllm_MATH_suffix_d5_no_global_o4k.sh
+++ recipe/srt/qwen_2_7b_vllm_MATH_suffix_d5_no_global_o4k.sh
@@ -9,6 +9,8 @@ math_test_path=/mnt/hdfs/ccchang_hldy/data/math/test.parquet
 train_files="['$math_train_path']"
 test_files="['$math_test_path']"
 
+use_dynamic_bsz=True
+
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files="$train_files" \
@@ -21,23 +23,23 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path="/mnt/hdfs/ccchang_hldy/Qwen2-7B-Instruct" \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=40 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.param_offload=False \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=40 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.disable_log_stats=False \
     actor_rollout_ref.rollout.n=5 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=40 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
+    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
+    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
     +actor_rollout_ref.rollout.engine_kwargs.vllm.speculative_config.method=suffix \
     +actor_rollout_ref.rollout.engine_kwargs.vllm.speculative_config.num_speculative_tokens=5 \
     +actor_rollout_ref.rollout.engine_kwargs.vllm.speculative_config.max_cached_requests=0 \
@@ -45,10 +47,10 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
     trainer.project_name='verl_grpo_example_gsm8k_math_cc' \
-    trainer.experiment_name='qwen2_7b_1104_suffix_default_d5_no_global_bs1024_o4k' \
+    trainer.experiment_name='qwen2_7b_1104_no_spec_bs1024_o4k' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
-    trainer.default_local_dir="/mnt/hdfs/__MERLIN_USER_DIR__/verl_ckpt/qwen2-7b_grpo_gsm8k_suffix_decode_no_global_bs1024" \
+    trainer.default_local_dir="/mnt/hdfs/__MERLIN_USER_DIR__/verl_ckpt/qwen2-7b_grpo_gsm8k_no_spec_bs1024_o4k" \
     trainer.total_epochs=15 $@
